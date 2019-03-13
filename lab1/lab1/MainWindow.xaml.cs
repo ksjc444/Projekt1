586388f6a13da76677323d2e78ce685dc25a77ef
Kamil Szprych <kkamil4sz@gmail.com>
2019-03-13 03:41:54 +0100

    Beta 1.0

--- lab1/lab1/MainWindow.xaml.cs
+++ lab1/lab1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,39 @@ namespace lab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Person> people = new ObservableCollection<Person>
+        {
+            new Person {FirstName = "Imię", SurName="Nazwisko", Photo= new BitmapImage(new Uri("person_image_sample.jpg", UriKind.Relative))}
+        };
+
+        public ObservableCollection<Person> Items
+        {
+            get => people;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog pictureDialog = new OpenFileDialog();
+            pictureDialog.Title = "Wybierz zdjęcie";
+            pictureDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+        "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+        "Portable Network Graphic (*.png)|*.png";
+
+            if (pictureDialog.ShowDialog() == true)
+            {
+                BigImageDisplay.Source = new BitmapImage(new Uri(pictureDialog.FileName));
+            }
+        }
+
+        private void AddPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new Person { FirstName = FirstNameTextBox.Text, SurName = SurNameTextBox.Text, Photo=BigImageDisplay.Source });
         }
     }
 }
---
+++ lab1/lab1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace lab1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public ImageSource Photo { get; set; }
+    }
+}
